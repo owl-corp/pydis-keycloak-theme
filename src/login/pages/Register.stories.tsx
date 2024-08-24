0d39251aca628161d90a7b8e5629d47444657a2b
Joe Banks <joe@jb3.dev>
2024-08-24 03:00:12 +0100

    Updates

--- src/login/pages/Register.stories.tsx
+++ src/login/pages/Register.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import type { Meta, StoryObj } from "@storybook/react";
 import { createKcPageStory } from "../KcPageStory";
 import type { Attribute } from "../../../dist/login";
