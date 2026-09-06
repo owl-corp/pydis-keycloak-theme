cb727d00c6ab0a9ab5b80bcf762f12b167bdaf94
Joe Banks <joe@jb3.dev>
2026-09-06 05:29:13 +0100

    Enrol language

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,6 +3,14 @@ import type { ThemeName } from "../kc.gen";
 
 const { useI18n, ofTypeI18n } = i18nBuilder
     .withThemeName<ThemeName>()
+    .withExtraLanguages({
+        xn: {
+            label: "Arnie",
+            getMessages: async () => ({
+                default: {} as any
+            })
+        }
+    })
     .build()
 
 type I18n = typeof ofTypeI18n;
