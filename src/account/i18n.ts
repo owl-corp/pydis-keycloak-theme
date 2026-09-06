2ce552d7edf447a2a98f0999ac4a3d5d0af62411
Joe Banks <joe@jb3.dev>
2026-09-06 06:14:03 +0100

    new localisation

---
+++ src/account/i18n.ts
@@ -0,0 +1,20 @@
+import { i18nBuilder  } from "keycloakify/account";
+import type { ThemeName } from "../kc.gen";
+
+const { useI18n, ofTypeI18n } = i18nBuilder
+    .withThemeName<ThemeName>()
+    .withExtraLanguages({
+        xn: {
+            label: "Arnie",
+            getMessages: () => import("./i18.xn")
+        }
+    })
+    .withCustomTranslations({
+        en: {
+            locale_xn: "Arnie",
+        }
+    })
+    .build()
+
+type I18n = typeof ofTypeI18n;
+export { useI18n, type I18n };
